9ffef1702eb7d645504f6b892df685c9efeef7d3
Shogo Ohta <athos0220@gmail.com>
2021-05-06 23:29:50 +0900

    Add more tests

--- reversi/board/board_test.go
+++ reversi/board/board_test.go
@@ -6,6 +6,52 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestRowsAndCols(t *testing.T) {
+	b23 := NewBoard(2, 3)
+	assert.Equal(t, 2, b23.Rows())
+	assert.Equal(t, 3, b23.Cols())
+	b44 := NewBoard(4, 4)
+	assert.Equal(t, 4, b44.Rows())
+	assert.Equal(t, 4, b44.Cols())
+}
+
+func TestGetAndSet(t *testing.T) {
+	b := NewBoard(4, 4)
+	assert.Equal(t, Empty, b.MustGetCell(&Pos{Y: 1, X: 1}))
+	assert.Equal(t, Empty, b.MustGetCell(&Pos{Y: 1, X: 2}))
+	b.MustSetCell(&Pos{Y: 1, X: 1}, White)
+	assert.Equal(t, White, b.MustGetCell(&Pos{Y: 1, X: 1}))
+	assert.Equal(t, Empty, b.MustGetCell(&Pos{Y: 1, X: 2}))
+}
+
+func TestIsFull(t *testing.T) {
+	b := NewBoard(2, 2)
+	b.MustSetCell(&Pos{Y: 0, X: 0}, White)
+	b.MustSetCell(&Pos{Y: 0, X: 1}, Black)
+	b.MustSetCell(&Pos{Y: 1, X: 0}, Black)
+	assert.Equal(t, false, b.IsFull())
+	b.MustSetCell(&Pos{Y: 1, X: 0}, White)
+	assert.Equal(t, false, b.IsFull())
+	b.MustSetCell(&Pos{Y: 1, X: 1}, White)
+	assert.Equal(t, true, b.IsFull())
+}
+
+func TestCollectFlippables(t *testing.T) {
+	b := NewBoard(4, 4)
+	b.MustSetCell(&Pos{Y: 1, X: 1}, White)
+	b.MustSetCell(&Pos{Y: 1, X: 2}, White)
+	b.MustSetCell(&Pos{Y: 2, X: 0}, Black)
+	b.MustSetCell(&Pos{Y: 2, X: 1}, White)
+	b.MustSetCell(&Pos{Y: 2, X: 2}, White)
+	b.MustSetCell(&Pos{Y: 3, X: 2}, Black)
+	actual := b.collectFlippables(&Pos{Y: 0, X: 2}, Black)
+	expected := [][]Pos{
+		{Pos{Y: 1, X: 1}},
+		{Pos{Y: 1, X: 2}, Pos{Y: 2, X: 2}},
+	}
+	assert.Equal(t, expected, actual)
+}
+
 func TestBoardToString(t *testing.T) {
 	b := NewBoard(8, 8)
 	b.MustSetCell(&Pos{3, 3}, White)
@@ -23,21 +69,5 @@ func TestBoardToString(t *testing.T) {
 | | | | | | | | |
 | | | | | | | | |
 +-+-+-+-+-+-+-+-+`
-	assert.Equal(t, expected, actual)
-}
-
-func TestCollectFlippables(t *testing.T) {
-	b := NewBoard(4, 4)
-	b.MustSetCell(&Pos{Y: 1, X: 1}, White)
-	b.MustSetCell(&Pos{Y: 1, X: 2}, White)
-	b.MustSetCell(&Pos{Y: 2, X: 0}, Black)
-	b.MustSetCell(&Pos{Y: 2, X: 1}, White)
-	b.MustSetCell(&Pos{Y: 2, X: 2}, White)
-	b.MustSetCell(&Pos{Y: 3, X: 2}, Black)
-	actual := b.collectFlippables(&Pos{Y: 0, X: 2}, Black)
-	expected := [][]Pos{
-		{Pos{Y: 1, X: 1}},
-		{Pos{Y: 1, X: 2}, Pos{Y: 2, X: 2}},
-	}
 	assert.Equal(t, expected, actual)
 }
