86f02364188dedb61b3b8fbfb7038f362217418f
Shogo Ohta <athos0220@gmail.com>
2021-05-07 21:27:39 +0900

    Add more tests

--- reversi/board/board_test.go
+++ reversi/board/board_test.go
@@ -1,18 +1,60 @@
 package board
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func TestOpponentOf(t *testing.T) {
+	tests := []struct {
+		title   string
+		in, out Cell
+	}{
+		{"White", White, Black},
+		{"Black", Black, White},
+	}
+	for _, tt := range tests {
+		t.Run(tt.title, func(t *testing.T) {
+			assert.Equal(t, tt.out, OpponentOf(tt.in))
+		})
+	}
+	assert.Panics(t, func() {
+		OpponentOf(Empty)
+	}, "Empty cell does not have opponent")
+}
+
+func TestPosToString(t *testing.T) {
+	tests := []struct {
+		pos Pos
+		out string
+	}{
+		{Pos{Y: 0, X: 0}, "a1"},
+		{Pos{Y: 3, X: 2}, "c4"},
+	}
+	for _, tt := range tests {
+		title := fmt.Sprintf("(%d,%d) -> %s", tt.pos.Y, tt.pos.X, tt.out)
+		t.Run(title, func(t *testing.T) {
+			assert.Equal(t, tt.out, tt.pos.String())
+		})
+	}
+}
 func TestRowsAndCols(t *testing.T) {
-	b23 := NewBoard(2, 3)
-	assert.Equal(t, 2, b23.Rows())
-	assert.Equal(t, 3, b23.Cols())
-	b44 := NewBoard(4, 4)
-	assert.Equal(t, 4, b44.Rows())
-	assert.Equal(t, 4, b44.Cols())
+	tests := []struct {
+		title    string
+		row, col int
+	}{
+		{"2x3", 2, 3},
+		{"4x4", 4, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.title, func(t *testing.T) {
+			b := NewBoard(tt.row, tt.col)
+			assert.Equal(t, tt.row, b.Rows())
+			assert.Equal(t, tt.col, b.Cols())
+		})
+	}
 }
 
 func TestGetAndSet(t *testing.T) {
