96ac865ad1156f9c27bd2462502a0641779b9649
Shogo Ohta <athos0220@gmail.com>
2021-05-09 23:56:45 +0900

    WIP: Implement reader

---
+++ lisp/impl/reader.go
@@ -0,0 +1,172 @@
+package lisp
+
+import (
+	"bufio"
+	"errors"
+	"io"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+var delimiters = map[rune]bool {
+	'(': true,
+	')': true,
+	'\'': true,
+	'"': true,
+	',': true,
+}
+
+type Reader struct {
+	reader *bufio.Reader
+}
+
+func NewReader(reader io.Reader) *Reader {
+	return &Reader{bufio.NewReader(reader)}
+}
+
+func (r *Reader) readRune() (rune, error) {
+	c, _, err := r.reader.ReadRune()
+	if err != nil {
+		return 0, nil
+	}
+	return c, nil
+}
+
+func (r *Reader) peekRune() (rune, error) {
+	c, err := r.readRune()
+	if err != nil {
+		return 0, nil
+	}
+	err = r.reader.UnreadRune()
+	if err != nil {
+		return 0, nil
+	}
+	return c, nil
+}
+
+func (r *Reader) readWhile(pred func(rune) bool) (string, error) {
+	var sb strings.Builder
+	for r.reader.Buffered() > 0 {
+		c, err := r.readRune()
+		if err != nil {
+			return "", nil
+		}
+		if !pred(c) {
+			return sb.String(), nil
+		}
+		sb.WriteRune(c)
+	}
+	return sb.String(), nil
+}
+
+func (r *Reader) dropWhile(pred func(rune) bool) error {
+	for r.reader.Buffered() > 0 {
+		c, err := r.readRune()
+		if err != nil {
+			return err
+		}
+		if !pred(c) {
+			return nil
+		}
+	}
+	return nil
+}
+
+func (r *Reader) skipWhitespaces() error {
+	return r.dropWhile(unicode.IsSpace)
+}
+
+func (r *Reader) readNumber() (Object, error) {
+	var negative bool
+	c, err := r.readRune()
+	if err != nil {
+		return nil, nil
+	}
+	if c == '-' {
+		negative = true
+	}
+	digits, err := r.readWhile(unicode.IsDigit)
+	if err != nil {
+		return nil, err
+	}
+	if negative {
+		digits = "-" + digits
+	}
+	n, err := strconv.Atoi(digits)
+	if err != nil {
+		return nil, err
+	}
+	return n, nil
+}
+
+func (r *Reader) readSymbol() (Object, error) {
+	name, err := r.readWhile(func(c rune) bool {
+		return !delimiters[c] && !unicode.IsSpace(c)
+	})
+	if err != nil {
+		return nil, err
+	}
+	switch name {
+	case "t":
+		return true, nil
+	case "nil":
+		return nil, nil
+	default:
+		return &Symbol{name}, nil
+	}
+}
+
+func (r *Reader) readList() (Object, error) {
+	// discards preceding '('
+	r.readRune()
+	var elems []Object
+	for {
+		c, err := r.peekRune()
+		if err != nil {
+			return nil, err
+		}
+		if c == ')' {
+			r.readRune()
+			var ret Object = nil
+			for i := range elems {
+				ret = NewCons(elems[len(elems)-i-1], ret)
+			}
+			return ret, nil
+		}
+		elem, err := r.Read()
+		if err != nil {
+			return nil, err
+		}
+		elems = append(elems, elem)
+	}
+}
+
+func (r *Reader) Read() (Object, error) {
+	err := r.skipWhitespaces()
+	if err != nil {
+		return nil, err
+	}
+	if r.reader.Buffered() == 0 {
+		return nil, errors.New("unexpected EOF")
+	}
+	c, err := r.readRune()
+	if err != nil {
+		return nil, err
+	}
+	switch {
+	case c == '-' || unicode.IsDigit(c):
+		return r.readNumber()
+	case c == '(':
+		return r.readList()
+	case c == ')':
+		return nil, errors.New("unexpected )")
+	default:
+		return r.readSymbol()
+	}
+}
+
+func ReadFromString(input string) (Object, error) {
+	r := NewReader(strings.NewReader(input))
+	return r.Read()
+}
