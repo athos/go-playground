f07f9133d702cc4ad19cb2e7c0d612bf6bc80f38
Shogo Ohta <athos0220@gmail.com>
2021-05-11 00:09:39 +0900

    WIP: Implement compiler

---
+++ lisp/impl/compiler.go
@@ -0,0 +1,204 @@
+package lisp
+
+import (
+	"errors"
+	"fmt"
+)
+
+type CEnv = map[string]*Location
+
+type Compiler struct {
+	insns []Insn
+	cenv  CEnv
+	level int
+}
+
+func NewCompiler() *Compiler {
+	return &Compiler{cenv: CEnv{}}
+}
+
+func (c *Compiler) clone() *Compiler {
+	cenv := CEnv{}
+	for k, v := range c.cenv {
+		cenv[k] = v
+	}
+	return &Compiler{c.insns, cenv, c.level}
+}
+
+func (c *Compiler) pushInsn(op Op, operands []Operand) {
+	c.insns = append(c.insns, Insn{op, operands})
+}
+
+func (c *Compiler) compile(expr Object) error {
+	switch e := expr.(type) {
+	case nil:
+		c.pushInsn(NIL, nil)
+	case bool, int:
+		c.pushInsn(LDC, []Operand{e})
+	case *Symbol:
+		loc := c.cenv[e.name]
+		if loc == nil {
+			return fmt.Errorf("unknown variable: %s", e.name)
+		}
+		c.pushInsn(LD, []Operand{&Location{c.level - loc.level, loc.offset}})
+	case *Cons:
+		return c.compileList(e.car, e.cdr)
+	}
+	return nil
+}
+
+func (c *Compiler) compileList(car Object, cdr Object) error {
+	switch obj := car.(type) {
+	case *Symbol:
+		switch obj.name {
+		case "+":
+			return c.compileOp(2, cdr, ADD)
+		case "-":
+			return c.compileOp(2, cdr, SUB)
+		case "*":
+			return c.compileOp(2, cdr, MUL)
+		case "/":
+			return c.compileOp(2, cdr, DIV)
+		case "=":
+			return c.compileOp(2, cdr, EQ)
+		case "<":
+			return c.compileOp(2, cdr, LT)
+		case ">":
+			return c.compileOp(2, cdr, GT)
+		case "<=":
+			return c.compileOp(2, cdr, LTE)
+		case ">=":
+			return c.compileOp(2, cdr, GTE)
+		case "cons":
+			return c.compileOp(2, cdr, CONS)
+		case "car":
+			return c.compileOp(1, cdr, CAR)
+		case "cdr":
+			return c.compileOp(1, cdr, CDR)
+		case "null":
+			return c.compileOp(1, cdr, NULL)
+		case "atom":
+			return c.compileOp(1, cdr, ATOM)
+		case "if":
+			return c.compileIf(cdr)
+		case "lambda":
+			return c.compileLambda(cdr)
+		default:
+			return c.compileApplication(car, cdr)
+		}
+	case *Cons:
+		return c.compileApplication(car, cdr)
+	default:
+		return fmt.Errorf("%s is not applicable", ToString(car))
+	}
+}
+
+func (c *Compiler) takeArgs(n int, argList Object) ([]Object, error) {
+	ret, improper, err := ListToSlice(argList)
+	if improper != nil || err != nil {
+		return nil, errors.New("arglist must be proper list")
+	}
+	nargs := len(ret)
+	if nargs < n {
+		return nil, errors.New("too less arguments")
+	} else if nargs > n {
+		return nil, errors.New("too many arguments")
+	}
+	return ret, nil
+}
+
+func (c *Compiler) compileOp(nargs int, argList Object, op Op) error {
+	args, err := c.takeArgs(nargs, argList)
+	if err != nil {
+		return err
+	}
+	for _, arg := range args {
+		if err := c.compile(arg); err != nil {
+			return err
+		}
+	}
+	c.pushInsn(op, nil)
+	return nil
+}
+
+func (c *Compiler) compileIf(argList Object) error {
+	args, err := c.takeArgs(3, argList)
+	if err != nil {
+		return err
+	}
+	c1 := c.clone()
+	c2 := c.clone()
+	if err := c.compile(args[0]); err != nil {
+		return err
+	}
+	if err := c1.compile(args[1]); err != nil {
+		return err
+	}
+	c1.pushInsn(JOIN, nil)
+	if err := c2.compile(args[2]); err != nil {
+		return err
+	}
+	c2.pushInsn(JOIN, nil)
+	c.pushInsn(SEL, []Operand{Code(c1.insns), Code(c2.insns)})
+	return nil
+}
+
+func (c *Compiler) compileLambda(argList Object) error {
+	args, err := c.takeArgs(2, argList)
+	if err != nil {
+		return err
+	}
+	cbody := c.clone()
+	cbody.level++
+	params, improper, err := ListToSlice(args[0])
+	if err != nil {
+		return err
+	}
+	if improper != nil {
+		return errors.New("arglist must be proper list")
+	}
+	for i, param := range params {
+		switch obj := param.(type) {
+		case *Symbol:
+			cbody.cenv[obj.name] = &Location{c.level, i}
+		default:
+			return errors.New("fn argument must be symbol")
+		}
+	}
+	if err := cbody.compile(args[1]); err != nil {
+		return err
+	}
+	cbody.pushInsn(RTN, nil)
+	c.pushInsn(LDF, []Operand{Code(cbody.insns)})
+	return nil
+}
+
+func (c *Compiler) compileApplication(fn Object, argList Object) error {
+	args, improper, err := ListToSlice(argList)
+	if err != nil {
+		return err
+	}
+	if improper != nil {
+		return errors.New("arglist must be proper list")
+	}
+	c.pushInsn(NIL, nil)
+	for _, arg := range args {
+		if err := c.compile(arg); err != nil {
+			return err
+		}
+		c.pushInsn(CONS, nil)
+	}
+	if err := c.compile(fn); err != nil {
+		return err
+	}
+	c.pushInsn(AP, nil)
+	return nil
+}
+
+func Compile(expr Object) (Code, error) {
+	compiler := NewCompiler()
+	if err := compiler.compile(expr); err != nil {
+		return nil, err
+	}
+	return compiler.insns, nil
+}
---
+++ lisp/impl/compiler_test.go
@@ -0,0 +1,151 @@
+package lisp
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompile(t *testing.T) {
+	tests := []struct {
+		in  Object
+		out Code
+	}{
+		{nil, Code{{NIL, nil}}},
+		{true, Code{{LDC, []Operand{true}}}},
+		{42, Code{{LDC, []Operand{42}}}},
+		{
+			&Cons{&Symbol{"+"}, &Cons{1, &Cons{2, nil}}},
+			Code{
+				{LDC, []Operand{1}},
+				{LDC, []Operand{2}},
+				{ADD, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"-"}, &Cons{5, &Cons{3, nil}}},
+			Code{
+				{LDC, []Operand{5}},
+				{LDC, []Operand{3}},
+				{SUB, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"*"}, &Cons{2, &Cons{3, nil}}},
+			Code{
+				{LDC, []Operand{2}},
+				{LDC, []Operand{3}},
+				{MUL, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"/"}, &Cons{8, &Cons{2, nil}}},
+			Code{
+				{LDC, []Operand{8}},
+				{LDC, []Operand{2}},
+				{DIV, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"="}, &Cons{1, &Cons{2, nil}}},
+			Code{
+				{LDC, []Operand{1}},
+				{LDC, []Operand{2}},
+				{EQ, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"<"}, &Cons{1, &Cons{2, nil}}},
+			Code{
+				{LDC, []Operand{1}},
+				{LDC, []Operand{2}},
+				{LT, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{">"}, &Cons{1, &Cons{2, nil}}},
+			Code{
+				{LDC, []Operand{1}},
+				{LDC, []Operand{2}},
+				{GT, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"<="}, &Cons{1, &Cons{2, nil}}},
+			Code{
+				{LDC, []Operand{1}},
+				{LDC, []Operand{2}},
+				{LTE, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{">="}, &Cons{1, &Cons{2, nil}}},
+			Code{
+				{LDC, []Operand{1}},
+				{LDC, []Operand{2}},
+				{GTE, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"null"}, &Cons{nil, nil}},
+			Code{
+				{NIL, nil},
+				{NULL, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"atom"}, &Cons{true, nil}},
+			Code{
+				{LDC, []Operand{true}},
+				{ATOM, nil},
+			},
+		},
+		{
+			&Cons{&Symbol{"if"}, &Cons{true, &Cons{1, &Cons{2, nil}}}},
+			Code{
+				{LDC, []Operand{true}},
+				{SEL, []Operand{
+					Code{{LDC, []Operand{1}}, {JOIN, nil}},
+					Code{{LDC, []Operand{2}}, {JOIN, nil}},
+				}},
+			},
+		},
+		// {
+		// 	&Cons{
+		// 		&Cons{
+		// 			&Symbol{"lambda"},
+		// 			&Cons{
+		// 				&Cons{&Symbol{"x"}, nil},
+		// 				&Cons{
+		// 					&Cons{
+		// 						&Symbol{"*"},
+		// 						&Cons{&Symbol{"x"}, &Cons{3, nil}},
+		// 					},
+		// 					nil,
+		// 				},
+		// 			},
+		// 		},
+		// 		&Cons{2, nil},
+		// 	},
+		// 	Code{
+		// 		{LDC, []Operand{2}},
+		// 		{LDF, []Operand{
+		// 			Code{
+		// 				{LD, []Operand{&Location{0, 0}}},
+		// 				{LDC, []Operand{3}},
+		// 				{MUL, nil},
+		// 				{RTN, nil},
+		// 			},
+		// 		}},
+		// 		{AP, nil},
+		// 	},
+		// },
+	}
+	for _, tt := range tests {
+		t.Run(ToString(tt.in), func(t *testing.T) {
+			code, err := Compile(tt.in)
+			assert.Equal(t, tt.out, code)
+			assert.Nil(t, err)
+		})
+	}
+}
