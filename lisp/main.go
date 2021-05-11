5db6229b028d8173d7dc963e570e9cc29827880c
Shogo Ohta <athos0220@gmail.com>
2021-05-11 22:36:24 +0900

    Implement simple REPL

--- lisp/main.go
+++ lisp/main.go
@@ -9,6 +9,23 @@ import (
 	lisp "github.com/athos/go-playground/lisp/impl"
 )
 
+func step(input string) (lisp.Object, error) {
+	obj, err := lisp.ReadFromString(input)
+	if err != nil {
+		return nil, err
+	}
+	code, err := lisp.Compile(obj)
+	if err != nil {
+		return nil, err
+	}
+	vm := lisp.NewVM(code)
+	v, err := vm.Run()
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
 func main() {
 	r := bufio.NewReader(os.Stdin)
 	for {
@@ -20,11 +37,11 @@ func main() {
 			}
 			panic(err)
 		}
-		obj, err := lisp.ReadFromString(input)
+		v, err := step(input)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			continue
 		}
-		fmt.Println(lisp.ToString(obj))
+		fmt.Println(lisp.ToString(v))
 	}
 }
